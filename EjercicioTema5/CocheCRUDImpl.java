956848d06b7664f00e91cf1089fe99ca6f9e7b91
Agustin Carrizo <jcarrizoagustin@gmail.com>
2022-09-12 16:49:38 -0300

    agregando anotadores

--- EjercicioTema5/CocheCRUDImpl.java
+++ EjercicioTema5/CocheCRUDImpl.java
@@ -3,19 +3,23 @@ package EjercicioTema5;
 public class CocheCRUDImpl implements CocheCRUD{
   
 
-  public CocheCRUDImpl(){
-
-  }
-
+  @Override
   public void save(){
     System.out.println("Metodo save");
   }
 
+  @Override
   public void findAll(){
     System.out.println("Metodo findAll");
   }
 
+  @Override
   public void delete(){
     System.out.println("Metodo delete");
+  }
+
+  @Override
+  public String toString(){
+    return "Clase CocheCRUDImpl que implementa la interfaz CocheCRUD";
   }
 }
