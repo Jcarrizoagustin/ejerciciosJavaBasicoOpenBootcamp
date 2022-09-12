f6c6774c1b7fd46ddf55f2c54e37b0fb972a9cac
Agustin Carrizo <jcarrizoagustin@gmail.com>
2022-09-12 16:45:24 -0300

    ejercicios tema 5

---
+++ EjercicioTema5/CocheCRUDImpl.java
@@ -0,0 +1,21 @@
+package EjercicioTema5;
+
+public class CocheCRUDImpl implements CocheCRUD{
+  
+
+  public CocheCRUDImpl(){
+
+  }
+
+  public void save(){
+    System.out.println("Metodo save");
+  }
+
+  public void findAll(){
+    System.out.println("Metodo findAll");
+  }
+
+  public void delete(){
+    System.out.println("Metodo delete");
+  }
+}
